13a88b8c04f9453d00dd5941c3483703386342e6
Martin-J-Larre <martinjlarre@gmail.com>
2023-12-15 23:23:14 -0500

    #5 Add redux/toolkit for handle global state user

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
 function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">User name</div>;
+  const userName = useSelector((state) => state.user.userName);
+
+  if (!userName) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
 
 export default UserName;
